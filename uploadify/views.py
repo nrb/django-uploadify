5b6e44e257bcc4f63bcad700030262615d04db84
Nolan Brubaker <nolan@nbrubaker.com>
2011-09-29 22:36:56 -0400

    Add user information to the signal.
    Provide the user object of the uploader so that receivers can use it
    when creating the new files.

--- uploadify/views.py
+++ uploadify/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 import django.dispatch
 
-upload_received = django.dispatch.Signal(providing_args=['data'])
+upload_received = django.dispatch.Signal(providing_args=['data', 'user'])
 
 def upload(request, *args, **kwargs):
     if request.method == 'POST':
         if request.FILES:
-            upload_received.send(sender='uploadify', data=request.FILES['Filedata'])
+            upload_received.send(sender='uploadify', data=request.FILES['Filedata'], user=request.user)
     return HttpResponse('True')
 
 
